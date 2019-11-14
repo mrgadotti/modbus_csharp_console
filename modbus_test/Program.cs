601c0d12d214cfda42c886fb4d777091706816c0
Marcelo Gadotti <gadotti.engenharia@gmail.com>
2019-11-14 19:08:33 -0200

    Initial commit

---
+++ modbus_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using EasyModbus;
+
+namespace modbus_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+            ModbusClient modbusClient = new ModbusClient("192.168.255.1", 502);
+            modbusClient.Connect();
+            modbusClient.WriteSingleCoil(8, true);
+            Thread.Sleep(1000);
+            modbusClient.WriteSingleCoil(8, false);
+            bool[] readInputs = modbusClient.ReadDiscreteInputs(0, 11);
+            for (int i = 0; i < readInputs.Length; i++)
+                Console.WriteLine("Value of input "+ readInputs[i].ToString());
+        }
+    }
+}
