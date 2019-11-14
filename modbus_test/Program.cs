62c735e43f6081cab881021aa893cb17d68b7685
Marcelo Gadotti <gadotti.engenharia@gmail.com>
2019-11-14 19:25:58 -0200

    Change to read discret inputs
    Signed-off-by: Marcelo Gadotti <gadotti.engenharia@gmail.com>

--- modbus_test/Program.cs
+++ modbus_test/Program.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading;
 using EasyModbus;
 
+/*
+ * http://easymodbustcp.net/codesampleseasymodbustcp-net
+ * https://sourceforge.net/projects/easymodbustcp/
+ */
+
 namespace modbus_test
 {
     class Program
@@ -17,9 +22,18 @@ namespace modbus_test
             modbusClient.WriteSingleCoil(8, true);
             Thread.Sleep(1000);
             modbusClient.WriteSingleCoil(8, false);
-            bool[] readInputs = modbusClient.ReadDiscreteInputs(0, 11);
-            for (int i = 0; i < readInputs.Length; i++)
-                Console.WriteLine("Value of input "+ readInputs[i].ToString());
+
+            while (true)
+            {
+                bool[] readInputs = modbusClient.ReadDiscreteInputs(0, 11);
+                Console.WriteLine("\r\n-----------------------------------------------------");
+                for (int i = 0; i < readInputs.Length; i++)
+                {
+                    Console.WriteLine("Value of input " + readInputs[i].ToString());
+                }
+                Console.WriteLine("-----------------------------------------------------");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
